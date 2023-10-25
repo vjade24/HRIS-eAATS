f206436a3701361a69f88db1d7810ed3dd46ebe9
vjade24 <vincenjade.vj@gmail.com>
2023-10-25 09:42:36 +0800

    insert lv history when manually add the monetization for printing

--- HRIS-eAATS/Controllers/cLeaveLedgerController.cs
+++ HRIS-eAATS/Controllers/cLeaveLedgerController.cs
@@ -570,7 +570,15 @@ namespace HRIS_eAATS.Controllers
                             db_ats.lv_ledger_dtl_tbl.Add(data_auto_vl);
                             db_ats.SaveChanges();
                         }
-                        
+                        // *************************************************************
+                        // **** VJA - 2023-06-01 -- Insert Leave Ledger History ********
+                        // *************************************************************
+                        var appl_status_mz = "Evaluated";
+                        var appl_remarks   = "Evaluate the Status for Printing";
+                        db_ats.sp_lv_ledger_history_insert(new_appl_nbr[0], "", data.empl_id, appl_status_mz, appl_remarks, Session["user_id"].ToString());
+                        // *************************************************************
+                        // **** VJA - 2023-06-01 -- Insert Leave Ledger History ********
+                        // *************************************************************
                     }
                 }
                 else if (data.leaveledger_entry_type == "1") // Automated Leave
@@ -760,15 +768,7 @@ namespace HRIS_eAATS.Controllers
                             db_ats.lv_ledger_dtl_tbl.Add(data_auto_vl);
                             db_ats.SaveChanges();
                         }
-                        // *************************************************************
-                        // **** VJA - 2023-06-01 -- Insert Leave Ledger History ********
-                        // *************************************************************
-                        var appl_status_mz = "Evaluated";
-                        var appl_remarks   = "Evaluate the Status for Printing";
-                        db_ats.sp_lv_ledger_history_insert(new_appl_nbr[0], "", data.empl_id, appl_status_mz, appl_remarks, Session["user_id"].ToString());
-                        // *************************************************************
-                        // **** VJA - 2023-06-01 -- Insert Leave Ledger History ********
-                        // *************************************************************
+                        
                     }
                 }
 
