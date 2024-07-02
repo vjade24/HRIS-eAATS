64041a52d84ba789c447f236c2fc5a284e9c10e7
Vincent Jade Alivio <vincentjade.vj@gmail.com>
2024-07-02 14:32:16 +0800

    leave and cto balances - export to excel

---
+++ HRIS-eAATS/Angular/ng-cLeaveCTOBalance.js
@@ -0,0 +1,187 @@
+﻿ng_HRD_App.controller("cLeaveCTOBalance_ctrlr", function ($scope, $compile, $http, $filter)
+{
+    var s       = $scope;
+    var h       = $http;
+    s.year      = [];
+    s.rowLen    = "10";
+    s.e
+    init()
+    function init()
+    {
+        $('#modal_generating').modal({ backdrop: 'static', keyboard: false });
+        h.post("../cLeaveCTOBalance/InitializeData").then(function (d)
+        {
+            if (d.data.message == "success")
+            {
+                RetrieveYear()
+                s.dep_lst = d.data.dep_lst
+                s.ddl_dep = d.data.dep_code
+
+                init_table_data([]);
+                s.oTable.fnClearTable();
+                s.datalistgrid = d.data.data;
+                if (d.data.data.length > 0)
+                {
+                    s.oTable.fnAddData(d.data.data);
+                }
+                $('#modal_generating').modal("hide");
+            }
+            else
+            {
+                swal(d.data.message, { icon: "error", });
+                $('#modal_generating').modal("hide");
+            }
+        })
+    }
+    s.FilterGrid = function ()
+    {
+        $('#modal_generating').modal({ backdrop: 'static', keyboard: false });
+        h.post("../cLeaveCTOBalance/FilterGrid", { year: s.ddl_year, dep_code: s.ddl_dep}).then(function (d)
+        {
+            if (d.data.message == "success")
+            {
+                s.oTable.fnClearTable();
+                s.datalistgrid = d.data.data;
+                if (d.data.data.length > 0)
+                {
+                    s.oTable.fnAddData(d.data.data);
+                }
+                $('#modal_generating').modal("hide");
+            }
+            else
+            {
+                swal(d.data.message, { icon: "error", });
+                $('#modal_generating').modal("hide");
+            }
+        })
+    }
+    var init_table_data = function (par_data) {
+        try {
+            s.datalistgrid = par_data;
+            s.oTable = $('#datalist_grid').dataTable(
+                {
+                    data: s.datalistgrid,
+                    bSort: true,
+                    bAutoWidth: false,
+                    sDom: 'rt<"bottom"ip>',
+                    columns: [
+                        {
+                            "mData": "empl_id",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-center btn-block'>" + data + "</span>"
+                            }
+                        },
+                        {
+                            "mData": "employee_name",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-center'>&nbsp;&nbsp;" + data + "</span>"
+                            }
+                        },
+                        {
+                            "mData": "department_short_name",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-center'>&nbsp;&nbsp;" + data + "</span>"
+                            }
+                        },
+                        {
+                            "mData": "monthly_rate",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-right btn-block'>" + currency(data) + "&nbsp;&nbsp;</span>"
+                            }
+                        },
+                        {
+                            "mData": "employment_type",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-center'>&nbsp;&nbsp;" + data + "</span>"
+                            }
+                        },
+                        {
+                            "mData": "vl_bal",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-center btn-block'>" + data + "</span>"
+                            }
+                        },
+                        {
+                            "mData": "sl_bal",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-center btn-block'>" + data + "</span>"
+                            }
+                        },
+                        {
+                            "mData": "sp_bal",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-center btn-block'>" + data + "</span>"
+                            }
+                        },
+                        {
+                            "mData": "cto_bal",
+                            "mRender": function (data, type, full, row) {
+                                return "<span class='small text-center btn-block'>" + data + "</span>"
+                            }
+                        },
+                    ],
+                    "createdRow": function (row, data, index) {
+                        $compile(row)($scope);  //add this to compile the DOM
+                    },
+
+                });
+        }
+        catch (err) {
+            swal({ icon: "warning", title: err.message });
+        }
+
+    }
+    function RetrieveYear()
+    {
+        var currentYear = new Date().getFullYear();
+        var prev_year = currentYear - 5;
+        for (var i = 1; i <= 8; i++)
+        {
+            var data = { "year": prev_year }
+            s.year.push(data)
+            prev_year++;
+        }
+        s.ddl_year  = new Date().getFullYear().toString();
+    }
+    function currency(d)
+    {
+        var retdata = ""
+        if (d == null || d == "" || d == undefined) {
+            return retdata = "0.00"
+        }
+        else {
+            retdata = d.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+            return retdata
+        }
+    }
+    s.search_in_list = function (value, table) {
+        try {
+            $("#" + table).DataTable().search(value).draw();
+        }
+        catch (err) {
+            alert(err.message)
+        }
+    }
+    s.setNumOfRow = function (value, table) {
+        try {
+            $("#" + table).DataTable().page.len(value).draw();
+        }
+        catch (err) {
+            alert(err.message)
+        }
+    }
+    s.fnExcelReport = function ()
+    {
+        var data = s.datalistgrid
+        // Convert object array to worksheet
+        const ws = XLSX.utils.json_to_sheet(data);
+
+        // Create a new workbook
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+
+        // Export the workbook
+        var filename = "leavebalances_" + moment().format('YYYYMMDD_hhmmss')+".xlsx";
+        XLSX.writeFile(wb, filename);
+    }
+})
---
+++ HRIS-eAATS/Controllers/cLeaveCTOBalanceController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using HRIS_eAATS.Models;
+
+namespace HRIS_eAATS.Controllers
+{
+    public class cLeaveCTOBalanceController : Controller
+    {
+        HRIS_DEVEntities db     = new HRIS_DEVEntities();
+        HRIS_ATSEntities db_ats = new HRIS_ATSEntities();
+        User_Menu um            = new User_Menu();
+        public ActionResult Index()
+        {
+            try
+            {
+                if (um != null || um.ToString() != "")
+                {
+                    um.allow_add            = (int)Session["allow_add"];
+                    um.allow_delete         = (int)Session["allow_delete"];
+                    um.allow_edit           = (int)Session["allow_edit"];
+                    um.allow_edit_history   = (int)Session["allow_edit_history"];
+                    um.allow_print          = (int)Session["allow_print"];
+                    um.allow_view           = (int)Session["allow_view"];
+                    um.url_name             = Session["url_name"].ToString();
+                    um.id                   = (int)Session["id"];
+                    um.menu_name            = Session["menu_name"].ToString();
+                    um.page_title           = Session["page_title"].ToString();
+                }
+                return View(um);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+        protected ActionResult JSON(object data, JsonRequestBehavior behavior)
+        {
+            return new JsonResult()
+            {
+                Data = data,
+                ContentType         = "application/json",
+                ContentEncoding     = Encoding.UTF8,
+                JsonRequestBehavior = behavior,
+                MaxJsonLength       = Int32.MaxValue
+            };
+        }
+        public ActionResult InitializeData()
+        {
+            try
+            {
+                var dep_code    = Session["department_code"].ToString();
+                var log_empl_id = Session["empl_id"].ToString();
+                var dep_lst     = db_ats.vw_leaveadmin_tbl_list.Where(a => a.empl_id == log_empl_id).OrderBy(a => a.department_code);
+                var data        = db_ats.sp_leave_balances_rep(DateTime.Now.Year.ToString(), dep_code).ToList();
+                return JSON(new { data, dep_lst, dep_code, message = "success" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                string message = e.Message.ToString();
+                return Json(new { message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult FilterGrid(string year,string dep_code)
+        {
+            try
+            {
+                var data        = db_ats.sp_leave_balances_rep(year, dep_code).ToList();
+                return JSON(new { data, message = "success" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                string message = e.Message.ToString();
+                return Json(new { message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
